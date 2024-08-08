b43da1b904dbf05f66cbe3051393acb9cb1fcd24
panwil <dipanfilo@users.noreply.github.com>
2024-08-08 14:03:03 +0800

    fix

--- components/remote_base/york_protocol.cpp
+++ components/remote_base/york_protocol.cpp
@@ -55,9 +55,10 @@ optional<YORKData> YORKProtocol::decode(RemoteReceiveData src) {
 
 
   uint32_t index = src.get_index();
-  int32_t value = src.operator[](index+1);
-  ESP_LOGI(TAG, "Received YORK: test data=%08" PRIX32, value);
-
+  ESP_LOGI(TAG, "Received YORK: test0 data=%d", src[index]);
+  ESP_LOGI(TAG, "Received YORK: test1 data=%d", src[index+1]);
+  ESP_LOGI(TAG, "Received YORK: test2 data=%d", src[index+2]);
+  ESP_LOGI(TAG, "Received YORK: test3 data=%d", src[index+3]);
 
   if (src.expect_item(BIT_HIGH_US, END_PULS)) {
     out.nbits = 1;
