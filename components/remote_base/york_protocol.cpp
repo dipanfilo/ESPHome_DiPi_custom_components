63cedfa347965a2bd6f8af996cae0ceaed9ce661
panwil <dipanfilo@users.noreply.github.com>
2024-08-08 16:55:59 +0800

    fix

--- components/remote_base/york_protocol.cpp
+++ components/remote_base/york_protocol.cpp
@@ -70,7 +70,9 @@ optional<YORKData> YORKProtocol::decode(RemoteReceiveData src) {
     return {};
   }
 
-  out.data = (((byte) recived_data[0]) << 56) | (((byte) recived_data[1]) << 48) | (((byte) recived_data[2]) << 40) | (((byte) recived_data[3]) << 32)| (((byte) recived_data[4]) << 24)| (((byte) recived_data[5]) << 16)| (((byte) recived_data[6]) << 8)| ((byte) recived_data[7]);
+  out.data  = (((byte) recived_data[0]) << 24)| (((byte) recived_data[1]) << 16)| (((byte) recived_data[2]) << 8)| ((byte) recived_data[3]);
+  out.data1 = (((byte) recived_data[4]) << 24)| (((byte) recived_data[5]) << 16)| (((byte) recived_data[6]) << 8)| ((byte) recived_data[7]);
+
 
   if (src.expect_item(BIT_HIGH_US, END_PULS)) {
     if (src.expect_mark(HEADER_HIGH_US)) {
@@ -78,12 +80,12 @@ optional<YORKData> YORKProtocol::decode(RemoteReceiveData src) {
     }
   }
 
-  SetDataFromBytes(recived_data);
+  SetDataFromBytes(&out, recived_data);
 
   return out;
 }
 void YORKProtocol::dump(const YORKData &data) {
-  ESP_LOGI(TAG, "Received YORK: data=0x%08" PRIX64 ", nbits=%d", data.data, data.nbits);
+  ESP_LOGI(TAG, "Received YORK: data=0x%08 %08" PRIX32 ", nbits=%d", data.data, data.data, data.nbits);
   ESP_LOGI(TAG, "Received YORK: currentTime=%d:%d", data.currentTime.hour, data.currentTime.minute);
   ESP_LOGI(TAG, "Received YORK: offTime=%d:%d active= %d", data.offTimer.hour, data.offTimer.halfHour, data.offTimer.active);
   ESP_LOGI(TAG, "Received YORK: offTime=%d:%d active= %d", data.onTimer.hour, data.onTimer.halfHour, data.onTimer.active);
