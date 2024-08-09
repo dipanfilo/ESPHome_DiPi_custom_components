9b8d5a017c6ff976e5b592e46c844d74c6e122e6
panwil <dipanfilo@users.noreply.github.com>
2024-08-09 21:21:08 +0800

    fix

--- components/climate_ir_york/climate_ir_york.h
+++ components/climate_ir_york/climate_ir_york.h
@@ -1,63 +1,12 @@
 #pragma once
 
 #include "esphome/components/climate_ir/climate_ir.h"
+#include "york_ir_typedef.h"
 
 #include <cinttypes>
 
 namespace esphome {
 namespace york {
-
-/*
- * The following typedef blocks define custom variable types which hold data
- * structures specific to this library.
- */
-
-// Temperature
-const uint8_t YORK_TEMP_MIN = 18;  // Celsius
-const uint8_t YORK_TEMP_MAX = 30;  // Celsius
-
-// The time_struct_t type is used to define a variable for storing the hour and
-// minute of the current time. This will be used to send the date/time to the
-// AC indoor unit.
-struct time_struct_t {
-    unsigned int hour;
-    unsigned int minute;
-};
-
-// The timer_struct_t type is used to define a variable to hold the auto on/off
-// data to send to the AC indoor unit. As with the remote controller, you can
-// only define a time with 30 minute increments.
-struct timer_struct_t {
-    unsigned int hour;
-    bool halfHour;
-    bool active;
-};
-
-// The OperationMode type is used to define a variable to hold the desired
-// operation mode of the AC.
-enum OperationMode {
-    OPERATION_MODE_DRY  = 0b0001,
-    OPERATION_MODE_COOL = 0b0010,
-    OPERATION_MODE_FAN  = 0b0100
-};
-
-// The FanMode type is used to define a variable to hold the fan mode of the
-// AC.
-enum FanMode {
-    FAN_MODE_AUTO           = 0b0001,
-    FAN_MODE_MANUAL_SPEED_3 = 0b0010,
-    FAN_MODE_MANUAL_SPEED_2 = 0b0100,
-    FAN_MODE_MANUAL_SPEED_1 = 0b1000,
-    FAN_MODE_QUIET          = 0b1001,
-    FAN_MODE_TURBO          = 0b0011,
-};
-
-// The SwingModeVertical is used to define a variable to hold the fan mode of the AC.
-enum SwingModeVertical {
-    SWING_MODE_VERTICAL_OFF  = 0b0000,
-    SWING_MODE_VERTICAL_AUTO = 0b0001,
-};
-
 
 class YorkClimate : public climate_ir::ClimateIR {
  public:
