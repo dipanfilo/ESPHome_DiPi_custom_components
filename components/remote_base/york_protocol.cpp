66910a92fe7e06efcda21cdb8d4495d052580a71
panwil <dipanfilo@users.noreply.github.com>
2024-08-11 16:01:20 +0800

    fix

--- components/remote_base/york_protocol.cpp
+++ components/remote_base/york_protocol.cpp
@@ -46,6 +46,7 @@ optional<YORKData> YORKProtocol::decode(RemoteReceiveData src) {
    
   byte recived_checksum = 0;
   byte calculated_checksum = 0; 
+  byte calculated_checksum2 = 0; 
 
   if (!src.expect_item(HEADER_HIGH_US, HEADER_LOW_US))
     return {};
@@ -66,19 +67,44 @@ optional<YORKData> YORKProtocol::decode(RemoteReceiveData src) {
 
   recived_checksum = selectLeftNibble(out.data[7]);
 
+  ESP_LOGI(TAG, "Received YORK data[0]=0x%02", out.data[0]);
+  ESP_LOGI(TAG, "Received YORK data[1]=0x%02", out.data[1]);
+  ESP_LOGI(TAG, "Received YORK data[2]=0x%02", out.data[2]);
+  ESP_LOGI(TAG, "Received YORK data[3]=0x%02", out.data[3]);
+  ESP_LOGI(TAG, "Received YORK data[4]=0x%02", out.data[4]);
+  ESP_LOGI(TAG, "Received YORK data[5]=0x%02", out.data[5]);
+  ESP_LOGI(TAG, "Received YORK data[6]=0x%02", out.data[6]);
+  ESP_LOGI(TAG, "Received YORK data[7]=0x%02", out.data[7]);
+  ESP_LOGI(TAG, "Received YORK recived checksum=0x%02", recived_checksum);
+  ESP_LOGI(TAG, "Received YORK");
+  ESP_LOGI(TAG, "Received YORK");
+
+ 
+
   //check the recived data checksum
   for (int i = 0; i < 8; i++) {
-    // Add reverse left nibble value
-    calculated_checksum += selectRightNibble(out.data[i]);
     // Add reverse right nibble value
+    calculated_checksum += selectRightNibble(out.data[i]);
+    // Add reverse left nibble value
     if (i < 7)
       calculated_checksum += selectLeftNibble(out.data[i]);
+
+
+    ESP_LOGI(TAG, "Received YORK calc checksum=0x%02", calculated_checksum);
+
+     if (i < 7)
+      calculated_checksum2 += out.data[i];
+     else
+      calculated_checksum2 += selectRightNibble(out.data[i]);
   }
+
+  ESP_LOGI(TAG, "Received YORK calc checksum2=0x%02", calculated_checksum2);
+
   calculated_checksum = selectLeftNibble(calculated_checksum);
 
   if(!(recived_checksum == calculated_checksum)) {
-    ESP_LOGI(TAG, "Received YORK data dont have a valid checksum: calc_checksum=0x%1", calculated_checksum);
-    return {};
+    ESP_LOGI(TAG, "Received YORK data dont have a valid checksum: calc_checksum=0x%02", calculated_checksum);
+    //return {};
   }
 
   if ((!src.expect_item(BIT_HIGH_US, END_PULS)) || (!src.expect_mark(HEADER_HIGH_US))) {
