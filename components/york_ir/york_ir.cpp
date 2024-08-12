d80ae57b30773aa94c4f68ef9072a4f968689a54
panwil <dipanfilo@users.noreply.github.com>
2024-08-12 10:02:42 +0800

    fix

--- components/york_ir/york_ir.cpp
+++ components/york_ir/york_ir.cpp
@@ -111,7 +111,7 @@ void YorkIR::control(const climate::ClimateCall &call) {
 void YorkIR::transmit_(YorkData &data) {
   data.finalize();
   auto transmit = this->transmitter_->transmit();
-  ESP_LOGI(TAG, "Clime Transmit York: %s", data.to_string().c_str());
+  ESP_LOGI(TAG, "Encode York IR data: %s", data.to_string().c_str());
   remote_base::YorkProtocol().encode(transmit.get_data(), data);
   transmit.perform();
 }
